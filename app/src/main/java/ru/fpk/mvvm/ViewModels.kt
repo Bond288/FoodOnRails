9466eddf74dd62d2a0e0148f04566b7aa1f7054d
bondarenko <bondarenkoau@inetra.ru>
2019-09-28 15:06:00 +0700

    Corrected my remarks

--- app/src/main/java/ru/fpk/mvvm/ViewModels.kt
+++ app/src/main/java/ru/fpk/mvvm/ViewModels.kt
@@ -4,24 +4,20 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProviders
-
 import toothpick.Scope
 
-object ViewModels {
-
-    operator fun <T : ViewModel> get(fragment: Fragment, scope: Scope, modelClass: Class<T>): T {
+fun <T : ViewModel> Fragment.getViewModel(scope: Scope, modelClass: Class<T>): T {
         return ViewModelProviders
-            .of(fragment, DependencyInjectionFactory(scope))
+            .of(this, DependencyInjectionFactory(scope))
             .get(modelClass)
     }
 
-    operator fun <T : ViewModel> get(
-        activity: FragmentActivity,
+
+fun <T : ViewModel> FragmentActivity.getViewModel(
         scope: Scope,
         modelClass: Class<T>
     ): T {
         return ViewModelProviders
-            .of(activity, DependencyInjectionFactory(scope))
+            .of(this, DependencyInjectionFactory(scope))
             .get(modelClass)
     }
-}
