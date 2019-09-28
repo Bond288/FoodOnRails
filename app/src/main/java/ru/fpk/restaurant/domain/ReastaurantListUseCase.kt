1faf4145ce4eae9d24e27baacdb4f146cd62ee34
bondarenko <bondarenkoau@inetra.ru>
2019-09-28 15:47:48 +0700

    Added filtering of restaurants by category

--- app/src/main/java/ru/fpk/restaurant/domain/ReastaurantListUseCase.kt
+++ app/src/main/java/ru/fpk/restaurant/domain/ReastaurantListUseCase.kt
@@ -1,11 +1,18 @@
 package ru.fpk.restaurant.domain
 
 import io.reactivex.Single
+import ru.fpk.categories.data.Category
 import ru.fpk.restaurant.data.Restaurant
 import ru.fpk.restaurant.data.RestaurantsProvider
 import javax.inject.Inject
 
 class ReastaurantListUseCase @Inject constructor(private val provider: RestaurantsProvider) {
 
-    fun loadRestaurantList() : Single<List<Restaurant>> = provider.restaurants()
+    fun loadRestaurantList(category: Category): Single<List<Restaurant>> = provider.restaurants()
+        .map {
+            when (category.tag) {
+                "all" -> it
+                else -> it.filter { restaurant -> restaurant.categories.any { restaurantCategory -> restaurantCategory == category } }
+            }
+        }
 }
