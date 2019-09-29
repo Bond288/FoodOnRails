8667682384bf554ece8369749cac3b7f8b58cd7d
Valentin Makarov <altepunk23@yandex.ru>
2019-09-29 11:15:14 +0700

    navigation drawer

--- app/src/main/java/ru/fpk/ui/gallery/DiscountsFragment.kt
+++ app/src/main/java/ru/fpk/ui/gallery/DiscountsFragment.kt
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import ru.fpk.R
 
-class GalleryFragment : Fragment() {
+class DiscountsFragment : Fragment() {
 
-    private lateinit var galleryViewModel: GalleryViewModel
+    private lateinit var discountsViewModel: DiscountsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        galleryViewModel =
-            ViewModelProviders.of(this).get(GalleryViewModel::class.java)
+        discountsViewModel =
+            ViewModelProviders.of(this).get(DiscountsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_gallery, container, false)
         val textView: TextView = root.findViewById(R.id.text_gallery)
-        galleryViewModel.text.observe(this, Observer {
+        discountsViewModel.text.observe(this, Observer {
             textView.text = it
         })
         return root
